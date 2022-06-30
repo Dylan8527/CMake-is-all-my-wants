00cc52f524487bf48f7373ddd549f7c4a559bf9c
Dylan8527 <90959591+Dylan8527@users.noreply.github.com>
2022-06-30 20:23:23 +0800

    Delete cmake-template/build directory

--- cmake-template/build/CMakeFiles/math.dir/src/c/math/minus.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/math.dir/src/c/math/minus.c.obj: \
- C:\Users\Lenovo\Desktop\CUDAA\CMake_study\cmake-template\src\c\math\minus.c \
- C:/Users/Lenovo/Desktop/CUDAA/CMake_study/cmake-template/src/c/math/minus.h
